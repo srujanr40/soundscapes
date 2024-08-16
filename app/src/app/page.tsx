08e36447fd7b080061ed360c689689cbfbd925a8
Srujan <srujanr40@gmail.com>
2024-08-16 16:03:37 -0700

    better title position

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,4 +1,4 @@
-import NavbarDemo from "../components/NavigationBar/navigationbar"
+import NavbarDemo from '../components/NavigationBar/navigationbar'
 import Card from '../components/Card/card'
 import Footer from '../components/Footer/footer'
 
@@ -7,11 +7,13 @@ export default function Home() {
     <div className="min-h-screen w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex flex-col overflow-y-scroll">
       <div className="absolute pointer-events-none inset-0 flex dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <NavbarDemo />
-      <h1 className="container mx-auto my-36 text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-primary">
-        Backgrounds
-        <br />
-        text here
-      </h1>
+      <div className="flex justify-center items-center h-[90vh]">
+        <h1 className="text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-primary">
+          Backgrounds
+          <br />
+          text here
+        </h1>
+      </div>
       <Card />
       <Card />
       <Card />
@@ -19,5 +21,5 @@ export default function Home() {
       <Card />
       <Footer />
     </div>
-  );
+  )
 }
