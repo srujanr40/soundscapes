bd0d987cd9210d99b88e9edbc8bed75fd5106c72
Srujan <srujanr40@gmail.com>
2024-08-17 08:45:47 -0700

    better title v2

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="absolute pointer-events-none inset-0 flex dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <NavbarDemo />
       <div className="flex flex-col justify-center items-center h-[75vh]">
-        <h1 className="text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-primary">
+        <h1 className="text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-br from-purple-500 via-pink-400 to-orange-200">
           Soundscapes
         </h1>
         <h2 className="text-center text-xl sm:text-3xl font-medium relative z-20 bg-clip-text text-transparent bg-muted-foreground">
