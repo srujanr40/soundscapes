f57c0e42b75c95e3875fb81ac17cc24e0494d0af
Srujan <srujanr40@gmail.com>
2024-08-20 16:43:01 -0700

    card image on hover

--- app/src/components/ui/card-hover-effect.tsx
+++ app/src/components/ui/card-hover-effect.tsx
@@ -1,26 +1,26 @@
-import { cn } from "@/lib/utils";
-import { AnimatePresence, motion } from "framer-motion";
-import Link from "next/link";
-import { useState } from "react";
+import { cn } from '@/lib/utils'
+import { AnimatePresence, motion } from 'framer-motion'
+import Link from 'next/link'
+import { useState } from 'react'
 
 export const HoverEffect = ({
   items,
   className,
 }: {
   items: {
-    title: string;
-    description: string;
-    link: string;
-    src?: string;
-  }[];
-  className?: string;
+    title: string
+    description: string
+    link: string
+    src?: string
+  }[]
+  className?: string
 }) => {
-  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
 
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10",
+        'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10',
         className
       )}
     >
@@ -28,14 +28,14 @@ export const HoverEffect = ({
         <Link
           href={item?.link}
           key={item?.link}
-          className="relative group  block p-2 h-full w-full"
+          className="relative group block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
         >
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-pink-100 dark:bg-fuchsia-900/[0.8] block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-pink-100 dark:bg-fuchsia-900/[0.8] block rounded-3xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -49,64 +49,79 @@ export const HoverEffect = ({
               />
             )}
           </AnimatePresence>
-          <Card>
+          <Card hovered={hoveredIndex === idx} backgroundImage={item.src}>
             <CardTitle>{item.title}</CardTitle>
             <CardDescription>{item.description}</CardDescription>
           </Card>
         </Link>
       ))}
     </div>
-  );
-};
+  )
+}
 
 export const Card = ({
   className,
   children,
+  hovered,
+  backgroundImage,
 }: {
-  className?: string;
-  children: React.ReactNode;
+  className?: string
+  children: React.ReactNode
+  hovered?: boolean
+  backgroundImage?: string
 }) => {
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-gray-200 dark:bg-black border-2 border-transparent dark:border-white/[0.2] dark:group-hover:border-fuchsia-300 group-hover:border-fuchsia-400 relative z-20",
+        'rounded-2xl h-full w-full p-4 overflow-hidden bg-gray-200 dark:bg-black border-2 border-transparent dark:border-white/[0.2] dark:group-hover:border-fuchsia-300 group-hover:border-fuchsia-400 relative z-20',
         className
       )}
+      style={{
+        backgroundImage: hovered ? `url('${backgroundImage}')` : undefined,
+        backgroundSize: hovered ? 'cover' : undefined,
+        backgroundPosition: hovered ? 'center' : undefined,
+      }}
     >
       <div className="relative z-50">
         <div className="p-4">{children}</div>
       </div>
     </div>
-  );
-};
+  )
+}
+
 export const CardTitle = ({
   className,
   children,
 }: {
-  className?: string;
-  children: React.ReactNode;
+  className?: string
+  children: React.ReactNode
 }) => {
   return (
-    <h4 className={cn("text-zinc-900 dark:text-zinc-100 font-bold tracking-wide mt-4", className)}>
+    <h4
+      className={cn(
+        'text-zinc-900 dark:text-zinc-100 font-bold tracking-wide mt-4',
+        className
+      )}
+    >
       {children}
     </h4>
-  );
-};
+  )
+}
 export const CardDescription = ({
   className,
   children,
 }: {
-  className?: string;
-  children: React.ReactNode;
+  className?: string
+  children: React.ReactNode
 }) => {
   return (
     <p
       className={cn(
-        "mt-8 text-zinc-600 dark:text-zinc-100 tracking-wide leading-relaxed text-sm",
+        'mt-8 text-zinc-600 dark:text-zinc-100 tracking-wide leading-relaxed text-sm',
         className
       )}
     >
       {children}
     </p>
-  );
-};
+  )
+}
