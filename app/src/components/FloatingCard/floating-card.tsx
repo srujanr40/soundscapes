e39421e6a8e88426de3302cd570c90294e34ad51
Srujan <srujanr40@gmail.com>
2024-08-20 17:39:19 -0700

    removed title bar shadow, fixed navbar shadows and border colour

--- app/src/components/FloatingCard/floating-card.tsx
+++ app/src/components/FloatingCard/floating-card.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 export default function FloatingCard() {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-transparent relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl">
+      <CardBody className="bg-transparent relative group/card w-auto sm:w-[30rem] h-auto rounded-xl">
         <CardItem
           translateZ="100"
           className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -19,7 +19,7 @@ export default function FloatingCard() {
         </CardItem>
         <div className="flex justify-center items-center">
           <CardItem translateZ="30">
-            <h2 className="text-center text-xl sm:text-3xl font-medium relative z-20 bg-clip-text text-transparent bg-muted-foreground">
+            <h2 className="text-center text-xl sm:text-3xl font-medium relative z-20 bg-clip-text text-transparent bg-gradient-to-br from-slate-300 to-slate-700">
               head in the clouds
             </h2>
           </CardItem>
