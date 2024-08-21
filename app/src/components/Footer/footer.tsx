6d519ac4dc24d3238df0511aa615dae639fa10d5
Srujan <srujanr40@gmail.com>
2024-08-21 12:45:53 -0700

    footer text now has the correct formatting

--- app/src/components/Footer/footer.tsx
+++ app/src/components/Footer/footer.tsx
@@ -1,9 +1,17 @@
 export default function Footer() {
   return (
     <div className="container grid grid-cols-2 pt-20 mb-10">
-      <p className="justify-self-start text-sm text-muted-foreground z-30">
-        Built with love frustration, Srujan Rao @ 2024
-      </p>
+      <div className="flex justify-self-start text-sm text-muted-foreground z-30">
+        <p className="pr-1">
+          Built with 
+        </p>
+        <p className="line-through pr-1">
+          love
+        </p>
+        <p>
+          frustration, Srujan Rao @ 2024
+        </p>
+      </div>
       <p className="justify-self-end text-sm text-muted-foreground z-30">
         Powered by Next.js, TailwindCSS, and Aceternity
       </p>
